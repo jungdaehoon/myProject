7abdfe570f80af69271ea447cdf811ccbee841ca
jeoungdaehoon <star03287@gmail.com>
2023-03-27 11:16:29 +0900

    Initial Commit

---
+++ cereal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cereal
+//
+//  Created by DaeHoon Chung on 2023/03/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
