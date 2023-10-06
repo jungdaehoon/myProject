b2d3cad4bfb3ac16f25ba2e81ca5398ffc42e2a1
jeoungdaehoon <126732376+jeoungdaehoon@users.noreply.github.com>
2023-10-06 18:11:22 +0900

    - 재로페이 예외 처리를 추가 하였습니다. - 홈 페이지 경우 홈 버튼 영역을 결제 버튼으로 변경 하였습니다.

---
+++ Class/UI/Main/View/HomePayModeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomePayModeView.swift
+//  cereal
+//
+//  Created by OKPay on 2023/10/06.
+//
+
+import UIKit
+
+class HomePayModeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
